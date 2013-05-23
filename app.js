ef4548fb2cd297c115cfc569ac1c25ddd88f75db
Amir <amir.saboury@gmail.com>
2013-05-23 14:50:24 +0430

    added UI & socket.io connection

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
+var http = require('http')
   , fs = require('fs')
-
-app.listen(3000);
+  , bee = require("beeline");
+  
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+  fs.readFile(__dirname + '/terminal.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -17,9 +16,39 @@ function handler (req, res) {
   });
 }
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+var router = bee.route({ // Create a new router
+    "/static/`path...`":
+        bee.staticDir("./static/", {".txt": "text/plain",".js": "text/javascript", ".html": "text/html",".css": "text/css", ".xml": "text/xml"}), "/`user`/static/`path...`": function(req, res, tokens, values) {
+    },
+	"/": handler
 });
+
+var app = http.createServer(router);
+var io = require('socket.io').listen(app);
+
+
+var spawn = require('child_process').spawn;
+var ssh = spawn('sh');
+
+
+
+
+ssh.on('exit', function (data) {
+	console.error("EX");
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.on('stdin', function (data) {
+		console.log(data.data);
+		ssh.stdin.write(data.data.toString());
+	});
+	ssh.stdout.on('data', function (data) {
+		console.log(data);
+		socket.emit('stdout', { data: data.toString() });
+	});
+	ssh.stderr.on('data', function (data) {
+		socket.emit('stdout', { data: data.toString() });
+	});
+});
+
+app.listen(3000);
